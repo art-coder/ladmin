0449f098e2306ca34572b056b8d65c7a10443811
ArtCoder <duanyng@gmail.com>
2021-09-13 10:50:17 +0800

    no message

--- README.md
+++ README.md
@@ -1,8 +1,13 @@
-# composer create-project laravel/laravel example-app
-# cd example-app
-# composer require artcoder/ladmin
+# Installation
 
-composer.json
+```sh
+composer create-project laravel/laravel demo
+cd demo
+composer require artcoder/ladmin
+```
+
+```json
+// composer.json
 {
   "autoload": {
     "psr-4": {
@@ -11,26 +16,47 @@ composer.json
     }
   }
 }
-# mkdir modules
-# composer dump-autoload
+```
 
-config/auth.php
+```sh
+mkdir modules
+composer dump-autoload
+```
+
+```php
+// config/auth.php
 'providers' => [
   'users' => [
     'driver' => 'eloquent',
     'model' => Artcoder\Ladmin\Entities\User::class,
   ],
 ],
+```
 
-config/app.php
+```php
+// config/app.php
   'providers' => [
     ...
-
+    Artcoder\Ladmin\AdminServiceProvider::class,
   ],
+```
 
-.env
+```sh
+# .env
   DB_DATABASE=xxx
   DB_USERNAME=xxx
   DB_PASSWORD=xxx
+```
 
-# php artisan ladmin:setup
+```sh
+php artisan ladmin:setup
+# php artisan module:make Cms
+```
+
+```php
+// CmsController.php
+  ...
+  use use Artcoder\Ladmin\Http\Controllers\Controller as AdminController;
+  ...
+  class CmsController extends AdminController
+```
--- src/Database/Seeders/UsersTableSeeder.php
+++ src/Database/Seeders/UsersTableSeeder.php
@@ -27,14 +27,14 @@ class UsersTableSeeder extends Seeder
         if ($isDev) {
             $adminPass = bcrypt('123456');
         } else {
-            $adminPass = bcrypt('duanadmin1478963250');
+            $adminPass = bcrypt('artcoderadmin159753');
         }
         // super admin
         $this->model->create([
             'username'       => 'ArtCoder',
             'avatar'         => 'https://dss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=1430948590,2484535776&fm=26&gp=0.jpg',
-            'phone'          => '15599115866',
-            'email'          => '568041672@qq.com',
+            'phone'          => '13800138000',
+            'email'          => 'admin@artcoder.com',
             'is_super_admin' => 1,
             'status'         => 0,
             'password'       => $adminPass,
@@ -45,8 +45,8 @@ class UsersTableSeeder extends Seeder
             $this->model->create([
                 'username' => 'edit',
                 'avatar'   => 'https://dss2.bdstatic.com/70cFvnSh_Q1YnxGkpoWK1HF6hhy/it/u=2019419245,1715884093&fm=26&gp=0.jpg',
-                'phone'    => '13551223151',
-                'email'    => '1161881312@qq.com',
+                'phone'    => '13800138001',
+                'email'    => 'edit@artcoder.com',
                 'password' => $adminPass,
                 'status'   => 0,
             ]);
